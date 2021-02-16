6eaa23d37734dd5a92fb96113443d3c0f330c4a4
Paul Kohlhoff <paul.kohlhoff@gmail.com>
2021-02-16 15:33:22 -0500

    module 5

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,17 +2,19 @@
 <div>
   <h1> {{ title }}</h1>
   <ul>
-    <book-item v-for="book in books" :book="book" :key="book.title.split('').reduce((hash, char) => (((hash<<5)-hash)+char.charCodeAt(0)), '')">
+    <book-item v-for="book in books" :book="book" :key="book.title.split('').reduce((h, ch) => (((h<<5)-h)+ch.charCodeAt(0)), '')">
     </book-item>
   </ul>
+  <book-form @addBook='appendBook'></book-form>
 </div>
 </template>
 
 <script>
 import BookItem from './BookItem';
+import BookForm from './BookForm';
 export default {
   name: 'BookList',
-  components: { BookItem },
+  components: { BookItem, BookForm },
   data: () => ({
     title: "All Books",
     books:[
@@ -20,7 +22,12 @@ export default {
       {title: 'American Gods', author: 'Neil Gaiman'},
       {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
     ]
-  })
+  }),
+  methods: {
+    appendBook(bookTitle, bookAuthor) {
+      this.books.push({ title: bookTitle, author: bookAuthor })
+    }
+  }
 };
 </script>
 
